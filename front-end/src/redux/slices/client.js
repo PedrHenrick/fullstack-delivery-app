7b51e7fa63da4513ac0b953dabb7f21de9dbe1c9
Kamila Hydalgo <kamilahydalgo@gmail.com>
2022-08-23 16:44:25 -0300

    feat(front): login page and store created do start application

---
+++ front-end/src/redux/slices/client.js
@@ -0,0 +1,19 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//   email: '',
+// };
+
+// export const userSlice = createSlice({
+//   name: 'client',
+//   initialState,
+//   reducers: {
+//     changeName: (state, action) => {
+//       state.email = action.payload;
+//     },
+//   },
+// });
+
+// export const {
+//   changeName,
+// } = userSlice.actions;
