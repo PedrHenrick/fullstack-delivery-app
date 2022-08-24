45fc4e905119d4590704a125c6f80de043464db1
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 20:42:30 -0300

    feat: :sparkles: add routes in the app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const register = require('../routes/register');
+const errorHandler = require('../middleware/error');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use(errorHandler);
+app.use('/register', register);
 
 module.exports = app;
