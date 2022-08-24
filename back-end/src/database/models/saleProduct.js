9fe68259d30463a60676c5aeb3dd6f53b7356db0
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 16:05:15 -0300

    fix(models): :bug: name in the models

--- back-end/src/database/models/saleProduct.js
+++ back-end/src/database/models/saleProduct.js
@@ -1,26 +1,38 @@
 module.exports = (Sequelize, DataTypes) => {
-  const SaleProduct = Sequelize.define('SaleProducts', {
-    saleId: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
-      field: 'sale_id',
+  const SaleProduct = Sequelize.define(
+    "SalesProducts",
+    {
+      saleId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        field: "sale_id",
+      },
+      productId: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+        field: "product_id",
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
-    productId: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
-      field: 'product_id',
-    },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  }, { timestamps: false });
+    { timestamps: false }
+  );
 
   SaleProduct.associate = (models) => {
-    SaleProduct.belongsTo(models.Sales, {foreignKey: 'saleId', as: 'sales'})
-  };
-  SaleProduct.associate = (models) => {
-    SaleProduct.belongsTo(models.Products, {foreignKey: 'productId', as: 'products'})
+    models.Products.belongsToMany(models.Sales, {
+      foreignKey: "saleId",
+      as: "sales",
+      through: SaleProduct,
+      otherKey: 'productId'
+    });
+    models.Sales.belongsToMany(models.Products, {
+      foreignKey: "productId",
+      as: "products",
+      through: SaleProduct,
+      otherKey: 'saleId'
+    });
   };
 
   return SaleProduct;
