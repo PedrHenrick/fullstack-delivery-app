fa2d921a4a3b52c0fb9dfc7ec5c440ee74f02ece
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:54:03 -0300

    feat(joi): :sparkles: Adicionando schema de registro pro joi

--- back-end/src/api/middlewares/schems.js
+++ back-end/src/api/middlewares/schems.js
@@ -2,7 +2,14 @@ const joi = require('joi');
 
 const loginSchema = joi.object({
   email: joi.string().email().required(),
-  password: joi.string().min(1).required(),
+  password: joi.string().min(6).required(),
 });
 
-module.exports = { loginSchema };
+const registerSchema = joi.object({
+  password: joi.string().min(6).required(),
+  email: joi.string().email().required(),
+  name: joi.string().required(),
+  role: joi.string().optional(),
+});
+
+module.exports = { loginSchema, registerSchema };
