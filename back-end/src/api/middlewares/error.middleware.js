d8dcfc1b53177c2bd70d2d6f8dd77ff0ccdb30ff
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:52:52 -0300

    feat(middleware): :sparkles: Adicionando middleware de erro criado pelo Ítalo

--- back-end/src/api/middlewares/error.middleware.js
+++ back-end/src/api/middlewares/error.middleware.js
@@ -1,6 +1,14 @@
-const ErrorMiddleware = (error, _request, response, _next) => {
-  const { status, message } = error;
-  response.status(status).json({ message });
-};
+const { StatusCodes } = require('http-status-codes');
+const errorCatalog = require('../errors/catalog');
 
-module.exports = { ErrorMiddleware };
+module.exports = (err, _req, res, _next) => {
+  const messageAsErrorType = err.message;
+  const mappedError = errorCatalog[messageAsErrorType];
+
+  if (mappedError) {
+    return res.status(mappedError.httpStatus).json({ error: mappedError.error });
+  }
+
+  console.error(err);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'internal error' });
+};
