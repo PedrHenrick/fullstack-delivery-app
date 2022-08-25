0060a01ccea8aa7171242aa7329ad3f2a86d7aab
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 22:19:01 -0300

    chore: :memo: add status code

--- back-end/src/middleware/error.js
+++ back-end/src/middleware/error.js
@@ -1,12 +1,14 @@
-const { errorCatalog } = require('../errors/catalog');
+const errorCatalog = require('../errors/catalog');
+const { StatusCodes } = require('http-status-codes');
 
 module.exports = (err, _req, res, _next) => {
   const messageAsErrorType = err.message;
   const mappedError = errorCatalog[messageAsErrorType];
+
   if (mappedError) {
     const { httpStatus, error } = mappedError;
     return res.status(httpStatus).json({ error });
   }
   console.error(err);
-  return res.status(500).json({ message: 'internal error' });
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'internal error' });
 };
