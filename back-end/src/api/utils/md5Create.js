2afc70ad23001aa411e36bc6f2146d0e04f0cf38
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-24 23:04:51 -0300

    feat(md5): :sparkles: Criando arquivo md5Create
    Arquivo que irá criar ou validar um hash md5 para a senha

---
+++ back-end/src/api/utils/md5Create.js
@@ -0,0 +1,9 @@
+const md5 = require('md5');
+
+const passwordHash = (password) => md5(password);
+const passwordCompare = (password, passwordHash) => passwordHash === md5(password);
+
+module.exports = {
+  passwordHash,
+  passwordCompare,
+};
