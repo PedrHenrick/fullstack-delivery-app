a011eb154b16df5680cd44487879590b58872bce
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 22:19:50 -0300

    fix(service): :fire: remove transaction

--- back-end/src/services/registerService.js
+++ back-end/src/services/registerService.js
@@ -8,27 +8,18 @@ const jwtConfig = {
 };
 
 const registerService = async (payload) => {
-  const t = await sequelize.transaction();
-  try {
     const { password, email, name } = payload;
     const [{ id, role }, created] = await Users.findOrCreate(
       {
         where: { email },
         defaults: { email, name, password: passwordHash(password) },
-        transaction: t,
       },
     );
     if (!created) throw new Error('emailCadastrado');
     
     const token = jwt.sign({ id, role }, JWT_SECRET, jwtConfig);
 
-    await t.commit();
-
     return { token, id };
-  } catch (e) {
-    await t.rollback();
-    throw new Error('generalError');
-  }
 };
 
 module.exports = {
