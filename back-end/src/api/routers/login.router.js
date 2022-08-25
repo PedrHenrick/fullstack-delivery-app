235e856b4b0e9495050ef9849110b700d261f864
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:58:17 -0300

    feat(router): :sparkles: Adicionando rota /register no arquivo de rotas de login

--- back-end/src/api/routers/login.router.js
+++ back-end/src/api/routers/login.router.js
@@ -3,8 +3,7 @@ const { UserModel } = require('../models/UserModel');
 const { LoginService } = require('../services/loginServices');
 const { LoginController } = require('../controllers/loginController');
 const { validateMiddleware } = require('../middlewares/validate.middleware');
-const { loginSchema } = require('../middlewares/schems');
-const user = require('../../database/models/user'); 
+const { loginSchema, registerSchema } = require('../middlewares/schems');
 
 const loginRouter = Router();
 
@@ -13,9 +12,14 @@ const loginServiceInstance = new LoginService(userModelInstance);
 const loginControllerInstance = new LoginController(loginServiceInstance);
 
 loginRouter.post(
-  '/',
+  '/login',
   validateMiddleware(loginSchema),
-  (request, response) => loginControllerInstance.authenticate(request, response)
+  (request, response) => loginControllerInstance.authenticate(request, response),
+);
+loginRouter.post(
+  '/register',
+  validateMiddleware(registerSchema),
+  (request, response) => loginControllerInstance.register(request, response),
 );
 
 module.exports = { loginRouter };
