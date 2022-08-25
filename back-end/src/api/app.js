0593abf468976e96b813067c1a64bba440f52e91
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:59:44 -0300

    refactor(app): :fire: Removendo importações desnecessárias

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,11 @@
 require('express-async-errors');
 const express = require('express');
 const { router } = require('./routers/router');
-const { ErrorMiddleware } = require('./middlewares/error.middleware');
-const register = require('../routes/register');
-const errorHandler = require('../middleware/error');
+const ErrorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
 app.use(express.json());
 app.use(router);
-app.use('/register', register);
-app.use(errorHandler);
+app.use(ErrorMiddleware);
 
 module.exports = app;
