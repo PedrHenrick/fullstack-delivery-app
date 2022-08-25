715a7093f8ef5009853473bfc372cc73ff5d770c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:56:35 -0300

    feat(service): :sparkles: Adicionando função register e corrigindo bugs
    Adicionando novo formato de erros e corrigindo lint

--- back-end/src/api/services/loginServices.js
+++ back-end/src/api/services/loginServices.js
@@ -1,24 +1,33 @@
-const { ErrorHandle } = require('../class/errorHandle');
 const { generateJWTToken } = require('../utils/JWTToken');
-const { passwordCompare } = require('../utils/md5Create');
+const { passwordCompare, passwordHash } = require('../utils/md5Create');
 
 class LoginService {
-  constructor(model) { this._model = model }
+  constructor(model) { this.model = model; }
   
-  async authentication(email, password) {
+  async authentication({ email, password }) {
     console.log('entrei no service');
     
-    const hasUser = await this._model.findOne({ email });
+    const hasUser = await this.model.findOne({ email });
 
-    if (!hasUser) throw ErrorHandle(404, 'Usuário não encontrádo');
+    if (!hasUser) throw new Error('userNotFound');
 
     if (!passwordCompare(password, hasUser.password)) {
-      throw ErrorHandle(404, 'Senha incorreta'); 
-    };
+      throw new Error('incorretPassword'); 
+    }
 
     const token = generateJWTToken(JSON.stringify(hasUser));
     return { token };
-  };
+  }
+
+  async register({ password, email, name }) {
+    const defaultObject = { email, name, password: passwordHash(password) };
+    const [{ id, role }, created] = await this.model.findOrCreate({ email }, defaultObject);
+    
+    if (!created) throw new Error('emailCadastrado');
+    
+    const token = generateJWTToken(JSON.stringify({ id, role }));
+    return { token, id };
+  }
 }
 
 module.exports = { LoginService };
