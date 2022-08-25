3412a0688bc05c594f45fc5cd4c732d35fd817b2
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-24 23:10:42 -0300

    feat(joi): :sparkles: Adicionando schema de login para validação do joi

---
+++ back-end/src/api/middlewares/schems.js
@@ -0,0 +1,8 @@
+const joi = require('joi');
+
+const loginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(1).required(),
+});
+
+module.exports = { loginSchema };
