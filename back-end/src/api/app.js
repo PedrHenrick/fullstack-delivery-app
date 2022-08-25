d2e2f73bac72d027f515cac4b0246b4a0b163f57
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 22:19:32 -0300

    fix: :bug: inverter order

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const express = require('express');
 const register = require('../routes/register');
 const errorHandler = require('../middleware/error');
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.use(errorHandler);
 app.use('/register', register);
+app.use(errorHandler);
 
 module.exports = app;
