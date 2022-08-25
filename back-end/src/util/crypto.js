fb23cd56122333168ade36f97d61cf3daec1f649
Italo Andrade <italoandrade13@gmail.com>
2022-08-24 23:18:19 -0300

    fix(lint): :bug: rename parameter

--- back-end/src/util/crypto.js
+++ back-end/src/util/crypto.js
@@ -1,7 +1,7 @@
 const md5 = require('md5');
 
 const passwordHash = (password) => md5(password);
-const passwordCompare = (password, passwordHash) => passwordHash === md5(password);
+const passwordCompare = (password, passwordHashDb) => passwordHashDb === md5(password);
 
 module.exports = {
 passwordHash,
