4a60c708d80a033a4f72e67b61912c2efd5df587
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 15:20:42 -0300

    refactor(jwt): :fire: Removendo a chave de expiração do jwt

--- back-end/src/api/utils/JWTToken.js
+++ back-end/src/api/utils/JWTToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const SECRET = process.env.JWT_SECRET || 'lalaland';
-const jwtConfig = { expiresIn: '15m', algorithm: 'HS256' };
+const jwtConfig = { algorithm: 'HS256' };
 
 const generateJWTToken = (payload) => jwt.sign(payload, SECRET, jwtConfig);
 
