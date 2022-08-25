127cb6b72548d42dec233d0f861b34d3c3eed916
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-08-25 13:48:45 -0300

    fix: :bug: Alterando nome do parâmetro que era igual ao da função

--- back-end/src/api/utils/md5Create.js
+++ back-end/src/api/utils/md5Create.js
@@ -1,7 +1,7 @@
 const md5 = require('md5');
 
 const passwordHash = (password) => md5(password);
-const passwordCompare = (password, passwordHash) => passwordHash === md5(password);
+const passwordCompare = (password, hash) => hash === md5(password);
 
 module.exports = {
   passwordHash,
