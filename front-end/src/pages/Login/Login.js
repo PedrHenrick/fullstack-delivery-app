1d035e722611068ffbbf00d710b3027a1b900680
Kamila Hydalgo <kamilahydalgo@gmail.com>
2022-08-26 17:11:42 -0300

    feat(front): requirement 4 done

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { requestLogin } from '../../utils/requestLogin';
 
 import { changeEmail, changePassword } from '../../redux/slices/client';
 
