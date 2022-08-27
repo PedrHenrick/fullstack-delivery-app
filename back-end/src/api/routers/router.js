b2a1df6942ba59dd16ae379dc7b2f0482f7a811e
Italo Andrade <italoandrade13@gmail.com>
2022-08-27 10:41:39 -0300

    feat(router): :sparkles: add seller router in the index of the router

--- back-end/src/api/routers/router.js
+++ back-end/src/api/routers/router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const { loginRouter } = require('./login.router');
+const sellerRouter = require('./seller.router');
 
 const router = Router();
 
 router.use('/', loginRouter);
+router.use('/', sellerRouter);
 
 module.exports = { router };
