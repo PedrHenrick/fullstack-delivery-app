a03a41dcb3b37a4b1a6f17c6a2c336f658ad4466
Italo Andrade <italoandrade13@gmail.com>
2022-08-29 15:24:38 -0300

    fix(lint): :bug: fix rule lint

--- back-end/src/api/services/loginServices.js
+++ back-end/src/api/services/loginServices.js
@@ -2,17 +2,24 @@ const { generateJWTToken } = require('../utils/JWTToken');
 const { passwordCompare, passwordHash } = require('../utils/md5Create');
 
 class LoginService {
-  constructor(model) { this.model = model; }
-  
-  async authentication({ email, password }) {    
+  constructor(model) {
+    this.model = model;
+  }
+
+  async authentication({ email, password }) {
     const hasUser = await this.model.findOne({ where: { email } });
 
     if (!hasUser) throw new Error('userNotFound');
 
     if (!passwordCompare(password, hasUser.password)) {
-      throw new Error('incorrectPassword'); 
+      throw new Error('incorrectPassword');
     }
-    const token = generateJWTToken({ id: hasUser.id, name: hasUser.name, email: hasUser.email, role: hasUser.role });
+    const token = generateJWTToken({
+      id: hasUser.id,
+      name: hasUser.name,
+      email: hasUser.email,
+      role: hasUser.role,
+    });
     return { token, id: hasUser.id };
   }
 
@@ -22,7 +29,12 @@ class LoginService {
       defaults: { email, name, role, password: passwordHash(password) },
     });
     if (!created) throw new Error('emailCadastrado');
-    const token = generateJWTToken({ id: user.id, name: user.name, email: user.email, role: user.role });
+    const token = generateJWTToken({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    });
     return { token, id: user.id };
   }
 }
--- back-end/src/api/utils/JWTToken.js
+++ back-end/src/api/utils/JWTToken.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const fs = require('fs');
-const SECRET = fs.readFileSync('./jwt.evaluation.key', 'utf-8' );
+
+const SECRET = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
 const jwtConfig = { algorithm: 'HS256' };
 
 const generateJWTToken = (payload) => jwt.sign((payload), SECRET, jwtConfig);
