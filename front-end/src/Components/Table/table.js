a7a3649866d9cf8ffeb7153cdb70f84e82837840
Italo Andrade <italoandrade13@gmail.com>
2022-09-02 16:49:20 -0300

    feat: :sparkles: add component table

---
+++ front-end/src/Components/Table/table.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { requestUsersWithToken } from '../../utils/requests';
+
+export default function DenseTable() {
+  const [allUsers, setAllUsers] = useState([]);
+
+  const { token } = JSON.parse(localStorage.getItem('userData'));
+  requestUsersWithToken('/users', token).then((e) => setAllUsers(e));
+
+  return (
+    <table>
+      {allUsers.map(({ name, role, email, id }) => (
+        <tr key={ name }>
+          <td datatest-id={ `admin_manage__element-user-table-item-number-${id}` }>
+            {id}
+          </td>
+          <td datatest-id={ `admin_manage__element-user-table-name-${id}` }>
+            {name}
+          </td>
+          <td datatest-id={ `admin_manage__element-user-table-email-${id}` }>
+            {email}
+          </td>
+          <td datatest-id={ `admin_manage__element-user-table-role-${id}` }>
+            {role}
+          </td>
+          <td
+            datatest-id={ `admin_manage__element-user-table-remove-${id}` }
+            // onClick={ async () => await deleteUser({ id }, token) }
+          >
+            Excluir
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
+}
