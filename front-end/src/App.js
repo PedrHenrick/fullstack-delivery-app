4ecfea0eb2ad426db34313b9df81047214961b9b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-09-06 17:39:18 -0300

    feat(app): :sparkles: Adding new route customer/orders/:id

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import CustomerOrders from './pages/CustomerOrders/CustomerOrders';
 import CustomerCheckout from './pages/CustomerCheckout/CustomerCheckout';
 import CustomerProducts from './pages/CustomerProducts/CustomerProducts';
+import CustomerOrderDetails from './pages/CustomerOrderDetails/CustomerOrderDetails';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import AdminRegister from './pages/Admin/AdminRegister';
@@ -16,6 +17,7 @@ function App() {
       <Route path="/register" element={ <Register /> } />
       <Route path="/customer/products" element={ <CustomerProducts /> } />
       <Route path="/customer/orders" element={ <CustomerOrders /> } />
+      <Route path="customer/orders/:id" element={ <CustomerOrderDetails /> } />
       <Route path="/customer/checkout" element={ <CustomerCheckout /> } />
       <Route path="/admin/manage" element={ <AdminRegister /> } />
     </Routes>
