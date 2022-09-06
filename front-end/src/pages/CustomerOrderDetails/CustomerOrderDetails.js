c73e8a8dee0d715f78c259034ea6962b8ec8bc85
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-09-06 17:40:36 -0300

    feat: :sparkles: Creating order details

---
+++ front-end/src/pages/CustomerOrderDetails/CustomerOrderDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../../Components/Header/Header';
+
+function CustomerOrderDetails() {
+  return (
+    <section>
+      <Header />
+    </section>
+
+  );
+}
+
+export default CustomerOrderDetails;
