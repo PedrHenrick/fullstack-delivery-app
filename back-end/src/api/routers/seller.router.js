49150aa7e778cff4c636f83fcf36aaa720891f31
Italo Andrade <italoandrade13@gmail.com>
2022-09-06 22:31:13 -0300

    fix: adataçao para novas models

--- back-end/src/api/routers/seller.router.js
+++ back-end/src/api/routers/seller.router.js
@@ -3,16 +3,18 @@ const SellerService = require('../services/sellerServices');
 const SellerController = require('../controllers/sellerController');
 const { authenticateMiddleware } = require('../middlewares/authenticate.middleware');
 const Sales = require('../../database/models/sale');
+const Products = require('../../database/models/product');
+const SalesProduct = require('../../database/models/saleProduct');
 
 const sellerRouter = Router();
 
-const sellerServiceInstance = new SellerService(Sales);
+const sellerServiceInstance = new SellerService(Sales, Products, SalesProduct);
 const sellerControllerInstance = new SellerController(sellerServiceInstance);
-
-sellerRouter.get('/orders', authenticateMiddleware,
-  (req, res) => sellerControllerInstance.allSellerOrders(req, res));
 
 sellerRouter.get('/orders/:id', authenticateMiddleware,
   (req, res) => sellerControllerInstance.sellerOrderDetailsController(req, res));
+
+sellerRouter.get('/orders', authenticateMiddleware,
+  (req, res) => sellerControllerInstance.allSellerOrders(req, res));
 
 module.exports = sellerRouter;
