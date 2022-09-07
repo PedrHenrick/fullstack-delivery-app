5629ef2ac6d79363d1c1fdb79121b821e4a1534c
Italo Andrade <italoandrade13@gmail.com>
2022-09-06 22:30:51 -0300

    fix: return api in the backend

--- back-end/src/api/services/sellerServices.js
+++ back-end/src/api/services/sellerServices.js
@@ -1,15 +1,33 @@
 class SellerService {
-  constructor(model) { this.model = model; }
+  constructor(sales, products, saleProduct) { 
+    this.salesModel = sales;
+    this.productsModel = products;
+    this.salesProdutsModel = saleProduct;
+  }
   
   async allSellerOrders(sellerId) {
-    const orders = await this.model.findAll({ where: { sellerId } });
+    const orders = await this.salesModel.findAll({ where: { sellerId } });
     return orders;
   }
 
   async sellerOrderDetails(id, sellerId) {
-    const orderDetails = await this.model.findOne({ where: { id, sellerId } });
-    return orderDetails;
-  }
+    const orderDetails = await this.salesModel.findOne({ where: { id, sellerId } });
+
+    const SalesProducts = await this.salesProdutsModel.findAll({ 
+      where: { saleId: id }, raw: true,
+     });
+
+    const products = await Promise.all(SalesProducts.map(async ({ productId, quantity }) => {
+      const oneProduct = await this.productsModel.findOne({ 
+        where: { id: productId }, 
+        attributes: { exclude: 'urlImage' }, 
+        raw: true,
+      });
+
+      return { ...oneProduct, quantity };
+    }));
+    
+    return { ...orderDetails.dataValues, products };  }
 }
 
 module.exports = SellerService;
