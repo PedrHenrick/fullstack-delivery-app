3712ad6ae6c4ae3b8a5f9c12929858878fbb9e9f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-09-09 02:37:37 -0300

    fix: :bug: Adding formatDate function

--- front-end/src/pages/CustomerOrders/CustomerOrders.js
+++ front-end/src/pages/CustomerOrders/CustomerOrders.js
@@ -2,32 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header/Header';
 import { requestGetWithToken } from '../../utils/requests';
+import formatDate from '../../utils/serializeDate';
 
 function CustomerOrders() {
   const [orders, setOrders] = useState([]);
-  const NUMBER_8 = 8;
-  const NUMBER_4 = 4;
-  const NUMBER_2 = 2;
-  const NUMBER_0 = 0;
-  const NUMBER_5 = 5;
-
   const navigate = useNavigate();
 
   useEffect(() => {
     const productsOrders = async () => {
       const { token } = JSON.parse(localStorage.getItem('user'));
+      if (!token) navigate('/login');
       const arrProducts = await requestGetWithToken('/customer/orders', token);
       setOrders(arrProducts);
     };
     productsOrders();
   }, []);
-
-  function formatDate(date) {
-    const strData = `${date.substr(NUMBER_8, NUMBER_2)}`
-      + `/${date.substr(NUMBER_5, NUMBER_2)}`
-      + `/${date.substr(NUMBER_0, NUMBER_4)}`;
-    return strData;
-  }
 
   const goToDetails = (id) => {
     const getProduct = orders.filter((element) => element.id === id);
